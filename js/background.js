d0c7ab690a97f7b578c5e20c137f9a5a022e54e5
안현우 <dfwtt11@gmail.com>
2022-12-22 21:26:23 +0900

    news add/bacogrund img change

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"]; // background img 이름을 array에 넣어둠
+const images = ["0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png"]; // background img 이름을 array에 넣어둠
 
 //random으로 이미지 나타내기
 const chosenImage = images[Math.floor(Math.random() * images.length)];
